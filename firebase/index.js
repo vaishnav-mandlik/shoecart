b5358ab201610b502eba9114ad59e8f5b5e91700
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2023-12-05 15:26:18 +0530

    add firebase

---
+++ firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvgHJCIW6MDujwkMA4esZETflFDB_zf_Y",
+  authDomain: "shoecart-31d68.firebaseapp.com",
+  projectId: "shoecart-31d68",
+  storageBucket: "shoecart-31d68.appspot.com",
+  messagingSenderId: "292933094617",
+  appId: "1:292933094617:web:2e470c1c452919056d02c8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
