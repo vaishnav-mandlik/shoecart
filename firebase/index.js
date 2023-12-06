49c8a97f097bfd1be7ea82dba872980b0388f400
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2023-12-06 10:45:50 +0530

    add firebase

--- firebase/index.js
+++ firebase/index.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
