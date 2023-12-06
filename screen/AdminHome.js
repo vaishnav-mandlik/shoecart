cc5eb8451b491eac4c9ab4d19126273744c905f9
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2023-12-06 10:46:10 +0530

    add admin home screen

---
+++ screen/AdminHome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import ShowShoes from "../components/ShowShoes";
+
+const AdminHome = () => {
+  return (
+    <View className="flex-1">
+      <ShowShoes />
+    </View>
+  );
+};
+
+export default AdminHome;
