fe7a4634378572e7cb7801d701ef2016a1d03bea
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2023-12-06 10:46:24 +0530

    add user screen

---
+++ screen/UserHome.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+import LottieView from "lottie-react-native";
+const UserHome = () => {
+  return (
+    <View>
+      <Text>UserHome</Text>
+    </View>
+  );
+};
+
+export default UserHome;
